7bf31003de5de6099b82faad4c761e88943c2fd1
gre-ilya <ilya.grebenyuk.01@mail.ru>
2021-11-12 12:47:19 +0500

    First commit

--- Template/main.cpp
+++ Template/main.cpp
@@ -3,10 +3,56 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+using std::cout;
+using std::endl;
 
-using namespace std;
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
 
 int main()
 {
-	
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* main_window = glfwCreateWindow(1600, 900, "Main window", 0, 0);
+	if (main_window == 0) {
+		cout << "ERROR::Failed to create window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(main_window);
+	glfwSetFramebufferSizeCallback(main_window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		cout << "ERROR::Failed to load GLAD" << endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 1600, 900);
+	while (!glfwWindowShouldClose(main_window)) {
+		processInput(main_window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(main_window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
 }
